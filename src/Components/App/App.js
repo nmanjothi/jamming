36bda1c9d8f96bfc2ad78aaef685268c1cf7419f
nmanjothi <nmanjothi@yahoo.com>
2023-03-29 11:25:44 +0300

    task 36

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar.js';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = 
+      {
+        searchResults: [{name:'name1', artist:'artist1', album:'album1', id:1},{name:'name2', artist:'artist2', album:'album2', id:2},{name:'name3', artist:'artist3', album:'album3', id:3}],
+        playlistName: [{playlistName:'playlistName'}],
+        playlistTracks: [{name:'name1', artist:'artist1', album:'album1', id:1},{name:'name2', artist:'artist2', album:'album2', id:2},{name:'name3', artist:'artist3', album:'album3', id:3}]
+      }
+    
+
+
+
+  }
   render() {
     return (
       <div>
@@ -11,8 +26,8 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {/* <SearchResults/> */}
-            {/* <Playlist/> */}
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
