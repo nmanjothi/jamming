4a2c2d15e6c81784a7d29006d1284e2744bdd3ae
nmanjothi <nmanjothi@yahoo.com>
2023-03-30 18:14:27 +0300

    step 55 completed

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,6 +3,31 @@ import React from 'react'
 import './Track.css'
 
 class Track extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.addTrack=this.addTrack.bind(this)
+    this.removeTrack=this.removeTrack.bind(this)
+    
+  }
+
+  addTrack() {
+  this.props.onAdd(this.props.track)
+
+  }
+
+  removeTrack() {
+    this.props.onRemove(this.props.track)
+  }
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action" onClick={this.removeTrack}>-</button>
+    } else {
+      return <button className="Track-action" onClick={this.addTrack}>+</button>
+    }
+  }
+   
   render() {
     return (
       <div className="Track">
@@ -15,9 +40,8 @@ class Track extends React.Component {
     )
   }
   
-  renderAction() {
-    if (this.props.IsRemoval) { return <button className="Track-action">-</button> } else { return <button className="Track-action">+</button> }
-  }
+
+  
 }
 
 export default Track; 
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,13 +8,14 @@ class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-    {
-        this.props.tracks.map(track => {return <Track track={track} key={track.id} /> })
-    }
-   
-</div>
+                {
+                    this.props.tracks.map(track => { return <Track track={track} key={track.id}
+                        onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} onRemove={this.props.onRemove} /> })
+                }
+
+            </div>
         )
     }
-}
+} 
 
 export default TrackList;
